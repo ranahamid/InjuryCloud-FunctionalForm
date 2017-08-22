a22594b374603f06b8df3c2b504e0ab14848c59b
Shoriful Islam <shoriful.cse.ru@gmail.com>
2017-08-22 22:04:51 +0600

    changes

--- WebApplication1/Controllers/FunctionalFormController.cs
+++ WebApplication1/Controllers/FunctionalFormController.cs
@@ -126,12 +126,15 @@ namespace WebApplication1.Controllers
             {
                 foreach (var item in q)
                 {
-                    ListItm.Add(new SelectListItem
+                    if( item!=null && item.AnswerDesc != null &&  item.AnswerDesc!=string.Empty && item.Id.ToString() != string.Empty)
                     {
-                        Text = item.AnswerDesc,
-                        Value = item.Id.ToString(),
-                        Selected = (item.Id.ToString() == inputValue) ? true : false
-                    });
+                        ListItm.Add(new SelectListItem
+                        {
+                            Text = item.AnswerDesc,
+                            Value = item.Id.ToString(),
+                            Selected = (item.Id.ToString() == inputValue) ? true : false
+                        });
+                    }
 
                 }
             }
@@ -157,8 +160,21 @@ namespace WebApplication1.Controllers
             medicalHistory.BestDescribesPainStr = GetIdForAns(testId, 3);
             medicalHistory.PainFeelWorse = MakeSelectList(3, medicalHistory.BestDescribesPainStr);
 
+            //5,6,7
+
+            medicalHistory.BestDescribesPainrightNowStr = GetIdForAns(testId, 5);
+            medicalHistory.BestDescribesPainrightNow = MakeSelectList(5, medicalHistory.BestDescribesPainrightNowStr);
+
+            medicalHistory.WorstPainOverPastStr = GetIdForAns(testId, 6);
+            medicalHistory.WorstPainOverPast = MakeSelectList(6, medicalHistory.WorstPainOverPastStr);
+
+            medicalHistory.LeastPainOverPastStr = GetIdForAns(testId, 7);
+            medicalHistory.LeastPainOverPast = MakeSelectList(7, medicalHistory.LeastPainOverPastStr);
+
 
             medicalHistory.makesPainFeelBetter = GetAnsDesForAns(testId, 4);
+
+
 
             //medical history
             medicalHistory.HistoryOfCancerStr = GetIdForAns(testId, 8);
@@ -198,7 +214,149 @@ namespace WebApplication1.Controllers
             medicalHistory.inches = GetAnsDesForAns(testId, 25) != string.Empty ? Int32.Parse(GetAnsDesForAns(testId, 25)) : 0; //25
             medicalHistory.Email = GetAnsDesForAns(testId, 26); //26
             medicalHistory.Weight = GetAnsDesForAns(testId, 27) != string.Empty ? Int32.Parse(GetAnsDesForAns(testId, 27)) : 0; //27
-            medicalHistory.Today = GetAnsDesForAns(testId, 23) != string.Empty ? DateTime.Parse(GetAnsDesForAns(testId, 28)) : DateTime.Now;//28
+            medicalHistory.Today = GetAnsDesForAns(testId, 28) != string.Empty ? DateTime.Parse(GetAnsDesForAns(testId, 28)) : DateTime.Now;//28
+
+            //PERSONAL HEALTH HISTORY
+            medicalHistory.DoYouSmokeStr = GetIdForAns(testId, 29);
+            medicalHistory.DoYouSmoke = MakeSelectList(29, medicalHistory.DoYouSmokeStr);
+
+            medicalHistory.LastPhysicalExamStr = GetIdForAns(testId, 30);
+            medicalHistory.LastPhysicalExam = MakeSelectList(30, medicalHistory.LastPhysicalExamStr);
+
+            medicalHistory.ReachedMenopauseStr = GetIdForAns(testId, 31);
+            medicalHistory.ReachedMenopause = MakeSelectList(31, medicalHistory.ReachedMenopauseStr);
+
+            medicalHistory.PSATestWithinTheLastYearStr = GetIdForAns(testId, 32);
+            medicalHistory.PSATestWithinTheLastYear = MakeSelectList(32, medicalHistory.PSATestWithinTheLastYearStr);
+
+            medicalHistory.ThyroidDysfunctionStr = GetIdForAns(testId, 33);
+            medicalHistory.ThyroidDysfunction = MakeSelectList(33, medicalHistory.ThyroidDysfunctionStr);
+
+            medicalHistory.RheumatoidArthritisStr = GetIdForAns(testId, 34);
+            medicalHistory.RheumatoidArthritis = MakeSelectList(34, medicalHistory.RheumatoidArthritisStr);
+
+            medicalHistory.OsteoarthritisStr = GetIdForAns(testId, 35);
+            medicalHistory.Osteoarthritis = MakeSelectList(35, medicalHistory.OsteoarthritisStr);
+
+            medicalHistory.OsteoporosisStr = GetIdForAns(testId, 36);
+            medicalHistory.Osteoporosis = MakeSelectList(36, medicalHistory.OsteoporosisStr);
+
+            medicalHistory.CancerStr = GetIdForAns(testId, 37);
+            medicalHistory.Cancer = MakeSelectList(37, medicalHistory.CancerStr);
+
+            medicalHistory.StrokeStr = GetIdForAns(testId, 38);
+            medicalHistory.Stroke = MakeSelectList(38, medicalHistory.StrokeStr);
+
+            medicalHistory.HighCholesterolStr = GetIdForAns(testId, 39);
+            medicalHistory.HighCholesterol = MakeSelectList(39, medicalHistory.HighCholesterolStr);
+
+            medicalHistory.HighBloodPressureStr = GetIdForAns(testId, 40);
+            medicalHistory.HighBloodPressure = MakeSelectList(40, medicalHistory.HighBloodPressureStr);
+
+            medicalHistory.DiabetesPersonalHistoryStr = GetIdForAns(testId, 41);
+            medicalHistory.DiabetesPersonalHistory = MakeSelectList(41, medicalHistory.DiabetesPersonalHistoryStr);
+
+            //MEDICATIONS
+
+            medicalHistory.OTCStr = GetIdForAns(testId, 42);
+            medicalHistory.OTC = MakeSelectList(42, medicalHistory.OTCStr);
+
+            medicalHistory.PrescriptionNonSteroidalStr = GetIdForAns(testId, 43);
+            medicalHistory.PrescriptionNonSteroidal = MakeSelectList(43, medicalHistory.PrescriptionNonSteroidalStr);
+
+            medicalHistory.NarcoticPainMedicationsStr = GetIdForAns(testId, 44);
+            medicalHistory.NarcoticPainMedications = MakeSelectList(44, medicalHistory.NarcoticPainMedicationsStr);
+
+            //SURGERY
+
+            medicalHistory.HeadStr = GetIdForAns(testId, 45);
+            medicalHistory.Head = MakeSelectList(45, medicalHistory.HeadStr);
+
+            medicalHistory.CardiovascularPulmonaryStr = GetIdForAns(testId, 46);
+            medicalHistory.CardiovascularPulmonary = MakeSelectList(46, medicalHistory.CardiovascularPulmonaryStr);
+
+            medicalHistory.AbdominalStr = GetIdForAns(testId, 47);
+            medicalHistory.Abdominal = MakeSelectList(47, medicalHistory.AbdominalStr);
+
+            medicalHistory.GenitoUrinaryStr = GetIdForAns(testId, 48);
+            medicalHistory.GenitoUrinary = MakeSelectList(48, medicalHistory.GenitoUrinaryStr);
+
+            medicalHistory.SpineStr = GetIdForAns(testId, 49);
+            medicalHistory.Spine = MakeSelectList(49, medicalHistory.SpineStr);
+
+            medicalHistory.PastSurgeryHadPerformDailyActivitiesStr = GetIdForAns(testId, 57);
+            medicalHistory.PastSurgeryHadPerformDailyActivities = MakeSelectList(57, medicalHistory.PastSurgeryHadPerformDailyActivitiesStr);
+
+
+
+
+            //FAMILY HISTORY
+
+            medicalHistory.DiabetesFamilyHistoryStr = GetIdForAns(testId, 50);
+            medicalHistory.DiabetesFamilyHistory = MakeSelectList(50, medicalHistory.DiabetesFamilyHistoryStr);
+
+            medicalHistory.ThyroidDysfunctionFamilyHistoryStr = GetIdForAns(testId, 51);
+            medicalHistory.ThyroidDysfunctionFamilyHistory = MakeSelectList(51, medicalHistory.ThyroidDysfunctionFamilyHistoryStr);
+
+            medicalHistory.RheumatoidArthritisLupusPsoriaticStr = GetIdForAns(testId, 52);
+            medicalHistory.RheumatoidArthritisLupusPsoriatic = MakeSelectList(52, medicalHistory.RheumatoidArthritisLupusPsoriaticStr);
+
+            medicalHistory.OsteoarthritisFamilyHistoryStr = GetIdForAns(testId, 53);
+            medicalHistory.OsteoarthritisFamilyHistory = MakeSelectList(53, medicalHistory.OsteoarthritisFamilyHistoryStr);
+
+            medicalHistory.OsteoporosisFamilyHistoryStr = GetIdForAns(testId, 54);
+            medicalHistory.OsteoporosisFamilyHistory = MakeSelectList(54, medicalHistory.OsteoporosisFamilyHistoryStr);
+
+            medicalHistory.CancerFamilyHistoryStr = GetIdForAns(testId, 55);
+            medicalHistory.CancerFamilyHistory = MakeSelectList(55, medicalHistory.CancerFamilyHistoryStr);
+
+            medicalHistory.StrokeFamilyHistoryStr = GetIdForAns(testId, 56);
+            medicalHistory.StrokeFamilyHistory = MakeSelectList(56, medicalHistory.StrokeFamilyHistoryStr);
+
+
+            //Oswestry Neck Pain Questionnaire
+          
+
+            medicalHistory.Section1Str = GetIdForAns(testId, 58);
+            medicalHistory.Section1 = MakeSelectList(58, medicalHistory.Section1Str);
+
+            medicalHistory.Section2Str = GetIdForAns(testId, 59);
+            medicalHistory.Section2 = MakeSelectList(59, medicalHistory.Section2Str);
+
+            medicalHistory.Section3Str = GetIdForAns(testId, 60);
+            medicalHistory.Section3 = MakeSelectList(60, medicalHistory.Section3Str);
+
+            medicalHistory.Section4Str = GetIdForAns(testId, 61);
+            medicalHistory.Section4 = MakeSelectList(61, medicalHistory.Section4Str);
+
+            medicalHistory.Section5Str = GetIdForAns(testId, 62);
+            medicalHistory.Section5 = MakeSelectList(62, medicalHistory.Section5Str);
+
+            medicalHistory.Section6Str = GetIdForAns(testId, 63);
+            medicalHistory.Section6 = MakeSelectList(63, medicalHistory.Section6Str);
+
+            medicalHistory.Section7Str = GetIdForAns(testId, 64);
+            medicalHistory.Section7 = MakeSelectList(64, medicalHistory.Section7Str);
+
+            medicalHistory.Section8Str = GetIdForAns(testId, 65);
+            medicalHistory.Section8 = MakeSelectList(65, medicalHistory.Section8Str);
+
+            medicalHistory.Section9Str = GetIdForAns(testId, 66);
+            medicalHistory.Section9 = MakeSelectList(66, medicalHistory.Section9Str);
+
+            medicalHistory.Section10Str = GetIdForAns(testId, 67);
+            medicalHistory.Section10 = MakeSelectList(67, medicalHistory.Section10Str);
+
+            medicalHistory.Section11Str = GetIdForAns(testId, 68);
+            medicalHistory.Section11 = MakeSelectList(68, medicalHistory.Section11Str);
+
+            medicalHistory.OswestryName = GetAnsDesForAns(testId, 69); //69
+            medicalHistory.OswestryDate = GetAnsDesForAns(testId, 70) != string.Empty ? DateTime.Parse(GetAnsDesForAns(testId, 70)) : DateTime.Now; //70
+            medicalHistory.OswestryOSWSCORE = GetAnsDesForAns(testId, 71); //71
+            medicalHistory.OswestryPSCORE = GetAnsDesForAns(testId, 72); //12
+
+
+
 
 
             return View(medicalHistory);
@@ -213,7 +371,10 @@ namespace WebApplication1.Controllers
             model.BestDescribesPain = MakeSelectList(1, model.BestDescribesPainStr);
             model.BestDescribesFrequencyPain = MakeSelectList(2, model.BestDescribesPainStr);
             model.PainFeelWorse = MakeSelectList(3, model.BestDescribesPainStr);
-            
+
+            model.BestDescribesPainrightNow= MakeSelectList(5, model.BestDescribesPainrightNowStr);
+            model.WorstPainOverPast = MakeSelectList(6, model.WorstPainOverPastStr);
+            model.LeastPainOverPast = MakeSelectList(7, model.LeastPainOverPastStr);
             //medical history
 
             model.HistoryOfCancer = MakeSelectList(8, model.HistoryOfCancerStr);
@@ -226,7 +387,67 @@ namespace WebApplication1.Controllers
             model.Sex = MakeSelectList(18, model.SexStr);
             model.DominantHand = MakeSelectList(19, model.DominantHandStr);
 
+            //personal history
 
+
+            model.DoYouSmoke = MakeSelectList(29, model.DoYouSmokeStr);
+            model.LastPhysicalExam = MakeSelectList(30, model.LastPhysicalExamStr);
+            model.ReachedMenopause = MakeSelectList(31, model.ReachedMenopauseStr);
+            model.PSATestWithinTheLastYear = MakeSelectList(32, model.PSATestWithinTheLastYearStr);
+            model.ThyroidDysfunction = MakeSelectList(33, model.ThyroidDysfunctionStr);
+            model.RheumatoidArthritis = MakeSelectList(34, model.RheumatoidArthritisStr);
+            model.Osteoarthritis = MakeSelectList(35, model.OsteoarthritisStr);
+            model.Osteoporosis = MakeSelectList(36, model.OsteoporosisStr);
+            model.Cancer = MakeSelectList(37, model.CancerStr);
+            model.Stroke = MakeSelectList(38, model.StrokeStr);
+            model.HighCholesterol = MakeSelectList(39, model.HighCholesterolStr);
+            model.HighBloodPressure = MakeSelectList(40, model.HighBloodPressureStr);
+            model.DiabetesPersonalHistory = MakeSelectList(41, model.DiabetesPersonalHistoryStr);
+
+            //MEDICATIONS
+            model.OTC = MakeSelectList(42, model.OTCStr);
+            model.PrescriptionNonSteroidal = MakeSelectList(43, model.PrescriptionNonSteroidalStr);
+            model.NarcoticPainMedications = MakeSelectList(44, model.NarcoticPainMedicationsStr);
+
+
+            //SURGERY
+       
+            model.Head = MakeSelectList(45, model.HeadStr);
+            model.CardiovascularPulmonary = MakeSelectList(46, model.CardiovascularPulmonaryStr);
+            model.Abdominal = MakeSelectList(47, model.AbdominalStr);
+            model.GenitoUrinary = MakeSelectList(48, model.GenitoUrinaryStr);
+            model.Spine = MakeSelectList(49, model.SpineStr);
+            model.PastSurgeryHadPerformDailyActivities = MakeSelectList(57, model.PastSurgeryHadPerformDailyActivitiesStr);
+
+
+
+            //FAMILY HISTORY
+
+
+            model.DiabetesFamilyHistory = MakeSelectList(50, model.DiabetesFamilyHistoryStr);
+            model.ThyroidDysfunctionFamilyHistory = MakeSelectList(51, model.ThyroidDysfunctionFamilyHistoryStr);
+            model.RheumatoidArthritisLupusPsoriatic = MakeSelectList(52, model.RheumatoidArthritisLupusPsoriaticStr);
+            model.OsteoarthritisFamilyHistory = MakeSelectList(53, model.OsteoarthritisFamilyHistoryStr);
+            model.OsteoporosisFamilyHistory = MakeSelectList(54, model.OsteoporosisFamilyHistoryStr);
+            model.CancerFamilyHistory = MakeSelectList(55, model.CancerFamilyHistoryStr);
+            model.StrokeFamilyHistory = MakeSelectList(56, model.StrokeFamilyHistoryStr);
+
+            //Oswestry Neck Pain Questionnaire
+
+
+            model.Section1 = MakeSelectList(58, model.Section1Str);
+            model.Section2 = MakeSelectList(59, model.Section2Str);
+            model.Section3 = MakeSelectList(60, model.Section3Str);
+            model.Section4 = MakeSelectList(61, model.Section4Str);
+            model.Section5 = MakeSelectList(62, model.Section5Str);
+            model.Section6 = MakeSelectList(63, model.Section6Str);
+            model.Section7 = MakeSelectList(64, model.Section7Str);
+            model.Section8 = MakeSelectList(65, model.Section8Str);
+            model.Section9 = MakeSelectList(66, model.Section9Str);
+            model.Section10 = MakeSelectList(67, model.Section10Str);
+            model.Section11 = MakeSelectList(68, model.Section11Str);
+
+            
 
             if (!ModelState.IsValid)
             {
@@ -240,6 +461,12 @@ namespace WebApplication1.Controllers
             insertTestAnswerTbl(testId, 3, model.PainFeelWorseStr);
 
             insertTestAnswerTblDesc(testId, 4, model.makesPainFeelBetter);
+
+            insertTestAnswerTbl(testId, 5, model.BestDescribesPainrightNowStr);
+            insertTestAnswerTbl(testId, 6, model.WorstPainOverPastStr);
+            insertTestAnswerTbl(testId, 7, model.LeastPainOverPastStr);
+
+
 
             //medical history
 
@@ -265,6 +492,74 @@ namespace WebApplication1.Controllers
             insertTestAnswerTblDesc(testId, 26, model.Email);
             insertTestAnswerTblDesc(testId, 27, model.Weight.ToString());
             insertTestAnswerTblDesc(testId, 28, model.Today.ToString());
+
+            //personal history
+
+
+            insertTestAnswerTbl(testId, 29, model.DoYouSmokeStr);
+            insertTestAnswerTbl(testId, 30, model.LastPhysicalExamStr);
+            insertTestAnswerTbl(testId, 31, model.ReachedMenopauseStr);
+            insertTestAnswerTbl(testId, 32, model.PSATestWithinTheLastYearStr);
+            insertTestAnswerTbl(testId, 33, model.ThyroidDysfunctionStr);
+            insertTestAnswerTbl(testId, 34, model.RheumatoidArthritisStr);
+            insertTestAnswerTbl(testId, 35, model.OsteoarthritisStr);
+            insertTestAnswerTbl(testId, 36, model.OsteoporosisStr);
+            insertTestAnswerTbl(testId, 37, model.CancerStr);
+            insertTestAnswerTbl(testId, 38, model.StrokeStr);
+            insertTestAnswerTbl(testId, 39, model.HighCholesterolStr);
+            insertTestAnswerTbl(testId, 40, model.HighBloodPressureStr);
+            insertTestAnswerTbl(testId, 41, model.DiabetesPersonalHistoryStr);
+
+            //MEDICATIONS
+            insertTestAnswerTbl(testId, 42, model.OTCStr);
+            insertTestAnswerTbl(testId, 43, model.PrescriptionNonSteroidalStr);
+            insertTestAnswerTbl(testId, 44, model.NarcoticPainMedicationsStr);
+
+            //SURGERY
+
+            insertTestAnswerTbl(testId, 45, model.HeadStr);
+            insertTestAnswerTbl(testId, 46, model.CardiovascularPulmonaryStr);
+            insertTestAnswerTbl(testId, 47, model.AbdominalStr);
+            insertTestAnswerTbl(testId, 48, model.GenitoUrinaryStr);
+            insertTestAnswerTbl(testId, 49, model.SpineStr);
+            insertTestAnswerTbl(testId, 57, model.PastSurgeryHadPerformDailyActivitiesStr);
+
+
+
+
+            //FAMILY HISTORY
+
+            insertTestAnswerTbl(testId, 50, model.DiabetesFamilyHistoryStr);
+            insertTestAnswerTbl(testId, 51, model.ThyroidDysfunctionFamilyHistoryStr);
+            insertTestAnswerTbl(testId, 52, model.RheumatoidArthritisLupusPsoriaticStr);
+            insertTestAnswerTbl(testId, 53, model.OsteoarthritisFamilyHistoryStr);
+            insertTestAnswerTbl(testId, 54, model.OsteoporosisFamilyHistoryStr);
+            insertTestAnswerTbl(testId, 55, model.CancerFamilyHistoryStr);
+            insertTestAnswerTbl(testId, 56, model.StrokeFamilyHistoryStr);
+
+
+
+            //Oswestry Neck Pain Questionnaire
+
+            insertTestAnswerTbl(testId, 58, model.Section1Str);
+            insertTestAnswerTbl(testId, 59, model.Section2Str);
+            insertTestAnswerTbl(testId, 60, model.Section3Str);
+            insertTestAnswerTbl(testId, 61, model.Section4Str);
+            insertTestAnswerTbl(testId, 62, model.Section5Str);
+            insertTestAnswerTbl(testId, 63, model.Section6Str);
+            insertTestAnswerTbl(testId, 64, model.Section7Str);
+            insertTestAnswerTbl(testId, 65, model.Section8Str);
+            insertTestAnswerTbl(testId, 66, model.Section9Str);
+            insertTestAnswerTbl(testId, 67, model.Section10Str);
+            insertTestAnswerTbl(testId, 68, model.Section11Str);
+
+     
+
+            insertTestAnswerTblDesc(testId, 69, model.OswestryName);
+            insertTestAnswerTblDesc(testId, 70, model.OswestryDate.ToString());
+            insertTestAnswerTblDesc(testId, 71, model.OswestryOSWSCORE);
+            insertTestAnswerTblDesc(testId, 72, model.OswestryPSCORE);
+
 
             //load medical history
             return RedirectToAction("Index", "Home");
--- WebApplication1/Models/FunctionalForm.cs
+++ WebApplication1/Models/FunctionalForm.cs
@@ -14,16 +14,16 @@ namespace WebApplication1.Models
 
     public class Medical
     {
-        [Display(Name ="Test Id: ")]
+        [Display(Name = "Test Id: ")]
         public string TestId { get; set; }
 
-       // pain des
+        // pain des
         public ICollection<System.Web.Mvc.SelectListItem> BestDescribesPain { get; set; }
         public string BestDescribesPainStr { get; set; }
-       
+
         public ICollection<System.Web.Mvc.SelectListItem> BestDescribesFrequencyPain { get; set; }
         public string BestDescribesFrequencyPainStr { get; set; }
-       
+
         public ICollection<System.Web.Mvc.SelectListItem> PainFeelWorse { get; set; }
         public string PainFeelWorseStr { get; set; }
 
@@ -82,11 +82,11 @@ namespace WebApplication1.Models
 
         [Display(Name = "PIN (Enter Four Digits)")]
         public int Pin { get; set; }
-        
+
         [Display(Name = "Date of Birth)")]
         public DateTime DateOfBirth { get; set; }
 
-  
+
 
         [Display(Name = "feet")]
         [Range(2, 8, ErrorMessage = "Enter valid height in feet")]
@@ -109,13 +109,150 @@ namespace WebApplication1.Models
 
         public ICollection<System.Web.Mvc.SelectListItem> DominantHand { get; set; }
         public string DominantHandStr { get; set; }
-                
+
         public DateTime Today { get; set; }
 
-        //
+        ////PERSONAL HEALTH HISTORY
+
+        public ICollection<System.Web.Mvc.SelectListItem> DoYouSmoke { get; set; }
+        public string DoYouSmokeStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> LastPhysicalExam { get; set; }
+        public string LastPhysicalExamStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> ReachedMenopause { get; set; }
+        public string ReachedMenopauseStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> PSATestWithinTheLastYear { get; set; }
+        public string PSATestWithinTheLastYearStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> ThyroidDysfunction { get; set; }
+        public string ThyroidDysfunctionStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> RheumatoidArthritis { get; set; }
+        public string RheumatoidArthritisStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Osteoarthritis { get; set; }
+        public string OsteoarthritisStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Osteoporosis { get; set; }
+        public string OsteoporosisStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Cancer { get; set; }
+        public string CancerStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Stroke { get; set; }
+        public string StrokeStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> HighCholesterol { get; set; }
+        public string HighCholesterolStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> HighBloodPressure { get; set; }
+        public string HighBloodPressureStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> DiabetesPersonalHistory { get; set; }
+        public string DiabetesPersonalHistoryStr { get; set; }
+
+        //MEDICATIONS
+        public ICollection<System.Web.Mvc.SelectListItem> OTC { get; set; }
+        public string OTCStr { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> PrescriptionNonSteroidal { get; set; }
+        public string PrescriptionNonSteroidalStr { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> NarcoticPainMedications { get; set; }
+        public string NarcoticPainMedicationsStr { get; set; }
+
+        //SURGERY
+        public ICollection<System.Web.Mvc.SelectListItem> Head { get; set; }
+        public string HeadStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> CardiovascularPulmonary { get; set; }
+        public string CardiovascularPulmonaryStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Abdominal { get; set; }
+        public string AbdominalStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> GenitoUrinary { get; set; }
+        public string GenitoUrinaryStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Spine { get; set; }
+        public string SpineStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> PastSurgeryHadPerformDailyActivities { get; set; }
+        public string PastSurgeryHadPerformDailyActivitiesStr { get; set; }
 
 
 
+
+
+        //FAMILY HISTORY
+
+        public ICollection<System.Web.Mvc.SelectListItem> DiabetesFamilyHistory { get; set; }
+        public string DiabetesFamilyHistoryStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> ThyroidDysfunctionFamilyHistory { get; set; }
+        public string ThyroidDysfunctionFamilyHistoryStr { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> RheumatoidArthritisLupusPsoriatic { get; set; }
+        public string RheumatoidArthritisLupusPsoriaticStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> OsteoarthritisFamilyHistory { get; set; }
+        public string OsteoarthritisFamilyHistoryStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> OsteoporosisFamilyHistory { get; set; }
+        public string OsteoporosisFamilyHistoryStr { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> CancerFamilyHistory { get; set; }
+        public string CancerFamilyHistoryStr { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> StrokeFamilyHistory { get; set; }
+        public string StrokeFamilyHistoryStr { get; set; }
+
+        //Oswestry Neck Pain Questionnaire
+
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section1 { get; set; }
+        public string Section1Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section2 { get; set; }
+        public string Section2Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section3 { get; set; }
+        public string Section3Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section4 { get; set; }
+        public string Section4Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section5 { get; set; }
+        public string Section5Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section6 { get; set; }
+        public string Section6Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section7 { get; set; }
+        public string Section7Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section8 { get; set; }
+        public string Section8Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section9 { get; set; }
+        public string Section9Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section10 { get; set; }
+        public string Section10Str { get; set; }
+
+        public ICollection<System.Web.Mvc.SelectListItem> Section11 { get; set; }
+        public string Section11Str { get; set; }
+
+        [Display(Name ="Name")]
+        public string OswestryName {get;set;}
+
+        [Display(Name = "Date")]
+        public DateTime OswestryDate { get; set; }
+
+        [Display(Name = "OSW-SCORE")]
+        public string OswestryOSWSCORE { get; set; }
+
+
+        [Display(Name = "P-SCORE")]
+        public string OswestryPSCORE { get; set; }
 
     }
 
